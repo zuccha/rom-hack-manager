8cd35df8dd94cf44a68fcc272996aad01cfd7f52
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-04-10 16:23:57 +0200

    feat: apply patch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -35,7 +35,12 @@ fn validate_url(url: &str) -> Result<(), String> {
 }
 
 #[tauri::command]
-async fn download_hack(name: &str, directory_path: &str, url: &str, vanilla_rom_path: &str) -> Result<(), String> {
+async fn download_hack(
+  app_handle: tauri::AppHandle,
+  name: &str,
+  directory_path: &str,
+  url: &str,
+  vanilla_rom_path: &str) -> Result<(), String> {
   // Validate Directory
   match validate_directory_path(directory_path) {
     Err(e) => return Err(e),
@@ -74,18 +79,50 @@ async fn download_hack(name: &str, directory_path: &str, url: &str, vanilla_rom_
     Ok(r) => r
   };
 
-  // Patch
-  // let os = env::consts::OS;
+  // Retrieve Flips
+  let flips_command = match std::env::consts::OS {
+    "macos" => "flips-macos",
+    "windows" => "flips-windows.exe",
+    _ => ""
+  };
 
+  let flips_path = match app_handle
+    .path_resolver()
+    .resolve_resource(Path::new("resources").join(flips_command)) {
+    None => return Err("Failed to locate Flips".into()),
+    Some(p) => p
+  };
+
+  if !flips_path.exists() {
+    return Err("Failed to retrieve Flips".into())
+  }
+
+  // Patch
+  hack_directory_path.read_dir().unwrap()
+    .filter_map(|res| res.ok())
+    .map(|dir_entry| dir_entry.path())
+    .filter(|path| path.extension().map_or(false, |ext| ext == "bps"))
+    .for_each(|bps_path| {
+      let mut sfc_path = PathBuf::from(&bps_path);
+      sfc_path.set_extension("sfc");
+      std::process::Command::new(&flips_path)
+        .arg("--apply")
+        .arg(&bps_path)
+        .arg(&vanilla_rom_path)
+        .arg(&sfc_path)
+        .spawn()
+        .unwrap();
+    });
+
+  // Open hack directory
   let open_command = match std::env::consts::OS {
     "macos" => "open",
     "windows" => "explorer",
     _ => ""
   };
-  if open_command.is_empty() { return Ok(()) }
-  std::process::Command::new(open_command).arg(hack_directory_path).spawn().unwrap();
-
-  // Return
+  if !open_command.is_empty() {
+    std::process::Command::new(open_command).arg(hack_directory_path).spawn().unwrap();
+  }
   Ok(())
 }
 
