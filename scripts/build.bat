7240433f68425958e4afccb7e3d40a178a9ee5c7
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-07-20 02:06:27 +0200

    fix: uncomment tauri build step in build scripts

--- scripts/build.bat
+++ scripts/build.bat
@@ -13,7 +13,7 @@ SET DIRPATH_INSTALLER=".\rom-hack-manager_%VERSION%_win_installer"
 SET APPNAME_BUNDLE="ROM Hack Manager.exe"
 SET DIRPATH_BUNDLE=".\rom-hack-manager_%VERSION%_win"
 
-@REM npm run tauri build
+npm run tauri build
 
 if NOT EXIST .\releases MKDIR .\releases
 cd .\releases
